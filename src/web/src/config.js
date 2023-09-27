6bba812b8c8cb50c88b87648befd00f716c3bc9a
MariazelHdez <37812834+MariazelHdez@users.noreply.github.com>
2023-09-27 10:15:52 -0700

    Update config.js

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -188,4 +188,4 @@ export const sections = [
     },
 ];
 export const environment = process.env.NODE_ENV;
-export const apiBaseUrl=process.env.VUE_APP_API_URL||'http://35.164.49.219:3000';
+export const apiBaseUrl = (process.env.NODE_ENV == "test" || process.env.NODE_ENV == "production") ? "" : "http://localhost:3000";
