ac45883fdd3bbfb68c86067be87462caf270bcc1
Mariazel Hernández <mariazelhernandez@Mariazels-MacBook-Pro.local>
2024-03-07 10:08:03 -0700

    Update connection time

--- src/api/repository/oracle/UserRepository.ts
+++ src/api/repository/oracle/UserRepository.ts
@@ -34,7 +34,7 @@ export class UserRepository extends BaseRepository<UserDTO> {
             } as UserRolesOptionsDTO);
         });
 
-        return allRoles;
+        return allRoles; 
     }
 
 }
